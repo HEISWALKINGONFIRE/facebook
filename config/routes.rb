9d3dbc63436413c61df61a3586a938f553da09a5
ivanseven <sir.ivanyeong@gmail.com>
2016-11-07 14:03:54 +0800

    Added Post  feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
-  resources :users, :sessions
+  #resources :home, only:[:index]
+  resources :users, only:[:new, :create]
+  resources :sessions, only:[:new, :create]
+  resources :posts, only:[:create]
   get 'log_out', to: 'sessions#log_out'
 end
