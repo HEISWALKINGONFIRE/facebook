367d3532c47e5ffa0d480f98d4b123574d45ec21
ivanseven <sir.ivanyeong@gmail.com>
2016-11-07 13:38:35 +0800

    User Login Implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+	validates :name, :email, :password, :password_confirmation, presence: true
+	validates_confirmation_of :password
+	validates :email, uniqueness: { case_sensitive: false }
 end
